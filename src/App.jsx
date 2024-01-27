a4f5eb744e46ffdfccb94cdf1243fc9e6b8683f4
Yesmin Pizarro <yesmin.pizarro@gmail.com>
2024-01-27 11:27:01 -0300

    Agregando Listado.
    Co-authored-by: Alejandro Stuardo <alstuardo@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import Listado from './components/Listado'
 import Formulario from './components/Formulario'
 import Alerta from './components/Alerta'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BaseColaboradores } from './BaseColaboradores'
+import { useState } from 'react'
 
 const App = () => {
+  const [listaColaboradores, setListaColaboradores] = useState(BaseColaboradores)
+  const [colaboradoresFilter, setColaboradoresFilter] = useState(listaColaboradores)
+
   return (
     <main>
       <Buscador/>
-      <Listado/>
+      <Listado listaColaboradores={listaColaboradores} 
+      setListaColaboradores={setListaColaboradores}
+      colaboradoresFilter={colaboradoresFilter}
+      setColaboradoresFilter={setColaboradoresFilter} />  
       <Formulario />
       <Alerta />
     </main>
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,8 +1,35 @@
+import Table from 'react-bootstrap/Table'
 
+const Listado = ({listaColaboradores, setListaColaboradores, colaboradoresFilter, setColaboradoresFilter}) => {
+    const colaboradores = colaboradoresFilter.map((colaborador) => (
+        <tr className="align-middle"
+        key={colaborador.id}>
+        <td>{colaborador.id}</td>
+        <td>{colaborador.nombre}</td>
+        <td>{colaborador.correo}</td>
+        <td>{colaborador.edad}</td>
+        <td>{colaborador.cargo}</td>
+        <td>{colaborador.telefono}</td>
+        <td>Eliminar</td>
+        </tr>
+  ))
 
-const Listado = () => {
   return (
-    <div>Listado</div>
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Correo</th>
+          <th>Edad</th>
+          <th>Cargo</th>
+          <th>Teléfono</th>
+        </tr>
+      </thead>
+      <tbody>
+        {colaboradores}
+      </tbody>
+    </Table>
   )
 }
 
